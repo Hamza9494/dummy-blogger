70790eb9162a00506b4d943f4b557fa9320f1004
Arthur <baitichhamza@gmail.com>
2023-12-16 18:34:06 +0100

    signp functionality

--- src/Signup.js
+++ src/Signup.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 const Signup = () => {
+  const [inputFields, setInputFields] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password_confirm: "",
+  });
+  const handleChange = (e) => {};
+  const [error, setError] = useState({});
+  const [submit, setSubmit] = useState(false);
   return (
     <div className="signup">
-      <h2>Signup</h2>
+      <form className="signup-form">
+        <div className="form-control">
+          <label htmlFor="name">name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={inputFields.name}
+            onChange={handleChange}
+          />
+        </div>
+      </form>
     </div>
   );
 };
