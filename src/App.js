d8abfef63d29151cdafcdee8a06fa8bdad1ddd9c
Arthur <baitichhamza@gmail.com>
2023-12-17 15:05:16 +0100

    add create blog functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./Signup";
 import Login from "./Login";
+import Create from "./Create";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path="/signup" element={<Signup />}></Route>
             <Route path="/login" element={<Login />}></Route>
+            <Route path="/create" element={<Create />}></Route>
           </Routes>
         </div>
       </div>
--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,75 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Create = () => {
+  const [fields, setFields] = useState({
+    author: "",
+    title: "",
+    body: "",
+  });
+
+  const [submit, setSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    setFields({ ...fields, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmit(true);
+    if (submit) {
+      const blog = {
+        author: fields.author,
+        title: fields.title,
+        body: fields.body,
+      };
+      axios
+        .post("http://localhost/projects/blogs-api/blogs.php", blog, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        })
+        .then((res) => res.data)
+        .then((data) => console.log(data));
+    }
+  };
   return (
     <div className="create">
       <h2>Add a blog</h2>
+      <form className="create-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="author">author</label>
+          <input
+            type="text"
+            name="author"
+            id="author"
+            value={fields.author}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="title">title</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={fields.title}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="body">body</label>
+          <textarea
+            name="body"
+            id="body"
+            value={fields.body}
+            onChange={handleChange}
+          />
+        </div>
+        <button>Add blog</button>
+      </form>
     </div>
   );
 };
